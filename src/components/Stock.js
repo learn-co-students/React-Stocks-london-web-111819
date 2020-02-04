9417987836ad290b4bb5aee5a54a4c3f0d06216d
Lope Ariyo <lopeariyo@gmail.com>
2020-02-04 11:32:10 +0000

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Stock = () => (
-  <div>
-
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
-      </div>
+const Stock = props => (
+    <div>
+        <div onClick={() => props.handleClick(props.stock)} className="card">
+            <div className="card-body">
+                <h5 className="card-title">{props.stock.name}</h5>
+                <p className="card-text">{`${props.stock.ticker}: ${props.stock.price}`}</p>
+            </div>
+        </div>
     </div>
-
-
-  </div>
 );
 
-export default Stock
+export default Stock;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,31 +1,68 @@
-import React, { Component } from 'react';
-import StockContainer from './StockContainer'
-import PortfolioContainer from './PortfolioContainer'
-import SearchBar from '../components/SearchBar'
+import React, { Component } from "react";
+import StockContainer from "./StockContainer";
+import PortfolioContainer from "./PortfolioContainer";
+import SearchBar from "../components/SearchBar";
 
 class MainContainer extends Component {
+    state = {
+        stocks: [],
+        portfolioStocks: []
+    };
 
-  render() {
-    return (
-      <div>
-        <SearchBar/>
+    getStocks = () => {
+        fetch("http://localhost:3000/stocks")
+            .then(resp => resp.json())
+            .then(stocks => this.setState({ stocks }));
+    };
 
-          <div className="row">
-            <div className="col-8">
+    componentDidMount() {
+        this.getStocks();
+    }
 
-              <StockContainer/>
+    handleBuy = stockToAdd => {
+        if (this.state.portfolioStocks.includes(stockToAdd)) {
+            return
+        } else {
+            const updatePortfolioStocks = [
+                stockToAdd,
+                ...this.state.portfolioStocks
+            ];
+            this.setState({ portfolioStocks: updatePortfolioStocks });
+        }
+    };
 
+    handleSell = stockToRemove => {
+        const updatePortfolioStocks = [...this.state.portfolioStocks].filter(
+            stock => stock.id !== stockToRemove.id
+        );
+        this.setState({ portfolioStocks: updatePortfolioStocks });
+    };
+
+    render() {
+        const stocks = this.state.stocks;
+        const portfolioStocks = this.state.portfolioStocks;
+
+        return (
+            <div>
+                <SearchBar />
+
+                <div className="row">
+                    <div className="col-8">
+                        <StockContainer
+                            stocks={[...stocks]}
+                            handleClick={this.handleBuy}
+                        />
+                    </div>
+                    <div className="col-4">
+                        <PortfolioContainer
+                            portfolioStocks={[...portfolioStocks]}
+                            handleClick={this.handleSell}
+                        />
+                    </div>
+                </div>
             </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
-          </div>
-      </div>
-    );
-  }
-
+        );
+    }
 }
 
 export default MainContainer;
